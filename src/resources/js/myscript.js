3b642f80eccfec9d3f9f5286c39150dc1a73e67c
Alessandro Menezes <alessandroadm@live.com>
2020-06-30 01:00:28 -0300

    Feat(site) Finished introduction

--- src/resources/js/myscript.js
+++ src/resources/js/myscript.js
@@ -1,2 +1,8 @@
-const STAR = document.getElementById('star');
-gsap.to(STAR, { x: 200, duration:3, rotation:360, scale:2 });
+const STAR = document.getElementById('star1');
+const THREE_STARS = document.querySelectorAll('.star');
+
+gsap.to(STAR, { x: 200, duration: 3, rotation: 360, scale: 2 });
+
+//stagger:1 faz com que cada elemento do array seja iterado um por vez 
+gsap.to(THREE_STARS, {stagger:1, x: 200, duration: 3, rotation: 360 });
+
